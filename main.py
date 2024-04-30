2ab7b8c4fc4aa26038933b7df89dba0cd038d6bd
frankvp11 <frankvanpaassen3@gmail.com>
2024-04-30 13:16:55 -0400

    Added the ability to see stuff in a nice format

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ def generate_svg_from_element(element):
         svg = SVG(width, height)
         with svg:
             for child in element:
-                print(child.tag)
-                generate_svg_from_element(child)
+                # print(child.tag)
+                children = generate_svg_from_element(child)
+                svg.add_child(children)
         return svg
     
     if element.tag == "{http://www.w3.org/2000/svg}circle":
@@ -61,11 +62,16 @@ def generate_svg_from_element(element):
         return Oval(cx, cy, rx, ry, fill=fill)
     
     if element.tag == "{http://www.w3.org/2000/svg}g":
-        group = Group()
+        x = float(element.attrib.get("x", 0))
+        y = float(element.attrib.get("y", 0))
+
+        group = Group(x, y)
         with group:
             for child in element:
-                print(child.tag)
-                generate_svg_from_element(child)
+                # print(child.tag)
+                
+                children = generate_svg_from_element(child)
+                group.add_child(children)
         return group
 
     if element.tag == "{http://www.w3.org/2000/svg}text":
@@ -86,7 +92,85 @@ with ui.row():
     svg_instance
 
 
+
+
+
+
+def parser2(node, indent=0):
+    if type(node) == SVG:
+        print("SVG")
+        for child in node.children:
+            parser2(child, indent+2)
+        return
+    elif type(node) == Group:
+        print(" "*indent + "Group")
+        for child in node.children:
+            parser2(child, indent+2)
+        return
+    else:
+        print(" "*indent + str(type(node)))
+        return
+    
+def update_props(node, prop, value):
+    node._props[prop] = value
+    node.update()
+
+def open_sidedrawer(node):
+    print(node._props)
+    with ui.right_drawer() as drawer:
+        for prop in node._props:
+            with ui.row():
+                ui.label(prop)
+                ui.input("prop value", value=node._props[prop]).on("keydown.enter", lambda e, prop=prop : update_props(node, prop, e.sender.value))
+        ui.button("Close", on_click=lambda : drawer.delete())
+
+
+
+def create_svg_tree(node, indent, depth):
+    if type(node) == SVG:
+        group = Group(20 + indent*20, 20 + depth*30)
+        group.on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+        with group:
+            Circle(20 + indent*20, 20 + depth*30, 20, fill="red", opacity=0.5)#.on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+            Text(5 +indent*20, 20 + depth*30, "SVG", font_size=10, centered=True)#.on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+        
+        prev_depth = depth
+        for index, child in enumerate(node.children):
+                shapes, prev_depth = create_svg_tree(child, indent+1, prev_depth+1+index)
+
+        return group
+    
+
+    #if it's a group, we must recurse
+    elif type(node) == Group:
+        group = Group(20 + indent*20, 20 + depth*30)
+        group.on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+
+        with group:
+            Circle(20 + indent*20, 20 + depth*30, 20, fill="red", opacity=0.5)# .on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+            Text(5 +indent*20, 20 + depth*30, "Group", font_size=10, centered=True)# .on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+        
+        for index, child in enumerate(node.children):
+                create_svg_tree(child, indent+1, depth+1+index)
+        return group, depth+len(node.children) -1
+    
+    #Case where it's not a group (ie, circle, elipse, rect, etc)
+    else:
+        group = Group(20 + indent*20, 20 + depth*30)
+        group.on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+        with group:
+            Circle(20 + indent*20, 20 + depth*30, 20, fill="red", opacity=0.5)#.on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+            Text(5 +indent*20, 20 + depth*30, str(type(node)), font_size=10, centered=True)#.on("svg:pointerdown", lambda : open_sidedrawer(node), throttle=0.1)
+
+        return group, depth+1
+        
+
+
+
+with SVG(1000, 1000):
+    create_svg_tree(svg_instance, 0, 0)
+
+parser2(svg_instance)
+
 ui.run()
 
-
-# print(svg_instance.to_string())  # Output the generated SVG code
